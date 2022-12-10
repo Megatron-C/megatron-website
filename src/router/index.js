c8d16f595405127a10d20f59848668e42a17550a
mazin01 <mmmazin01@gmail.com>
2022-12-10 15:59:31 +0200

    🚀 🚀 🚀 🔥 🔥 🔥

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
 
-// var history = require('connect-history-api-fallback');
-
-// var connect = require('connect');
-
-// var app = connect()
-//   .use(history())
-//   .listen(3000);
-
-// var connect = require('connect')
-// var http = require('http')
-// var https = require('https');
-
-// var app = connect();
-// app
-
-// http.createServer(app).listen(80);
-// https.createServer(options, app).listen(443);
-
-
 const HomePage = () => import('../views/Home.vue')
 const WorkPage = () => import('../views/Work.vue')
 const CareersPage = () => import('../views/Careers.vue')
@@ -32,16 +13,7 @@ const routes = [
   { path: '/careers', component: CareersPage },
   { path: '/about', component: AboutPage },
   { path: '/contact', component: ContactPage },
-  { path: '/project/:id', component: Project },
-  // {
-  //   // path: "*",
-  //   path: "/:catchAll(.*)",
-  //   name: "NotFound",
-  //   component: PageNotFound,
-  //   meta: {
-  //     requiresAuth: false
-  //   }
-  // }
+  { path: '/project/:id', component: Project},
 ]
 
 const router = createRouter({
